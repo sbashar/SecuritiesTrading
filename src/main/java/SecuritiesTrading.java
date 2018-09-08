fed8561e941fd05288b54a10eefcec9f1b2e5301
Saydul Bashar <tamimnoone@gmail.com>
2018-09-08 23:42:58 +0100

    Reading csv files in spark

--- src/main/java/SecuritiesTrading.java
+++ src/main/java/SecuritiesTrading.java
@@ -1,20 +1,15 @@
-import org.apache.spark.api.java.function.FilterFunction;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class SecuritiesTrading {
     public static void main(String[] args) {
-        String logFile = "README.md";
-        SparkSession sparkSession = SparkSession.builder().appName("Simple Application").getOrCreate();
+        SparkSession spark = SparkSession.builder().appName("Securities Trading Application").getOrCreate();
 
-        Dataset<String> logData = sparkSession.read().textFile(logFile).cache();
+        Dataset<Row> securitiesTradingData = spark.read().option("header", true).option("inferSchema", true).csv("2018-06-29_BINS_XETR14.csv");
+        securitiesTradingData.show();
 
-        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
-        long numBs = logData.filter((FilterFunction<String>) s -> s.contains("b")).count();
-
-        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
-
-        sparkSession.stop();
+        spark.stop();
 
     }
 }
